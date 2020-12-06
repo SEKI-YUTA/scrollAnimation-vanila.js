7e76e3e0340bbf38743c2cab1f582241030bb654
YUTA <56211510+SEKI-YUTA@users.noreply.github.com>
2020-12-06 09:34:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const targetElement = document.querySelectorAll(".animationTarget");
+const targetElement = document.querySelectorAll(".animationTarget");  //animationTargetがついた要素を取得
 console.log("画面の高さ",window.innerHeight);
-document.addEventListener("scroll", function() {
-  for(let i = 0; i < targetElement.length; i++) {
-    const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6;
+document.addEventListener("scroll", function() {  //スクロールした時に実行
+  for(let i = 0; i < targetElement.length; i++) {  //要素の数だけ実行
+    const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6;  //要素の半分ぐらいまでの距離
+    //getBoundingClientReact().top: ブラウザの表示領域の一番上から要素の上までの距離
+
     // console.log(getElementDistance);
 
-    if(window.innerHeight > getElementDistance) {
+    if(window.innerHeight > getElementDistance) {  //もし半分見えたらshowクラスをつける
       targetElement[i].classList.add('show');
     }
   }
